85d8109c08b9cc5a17880bb29c62da688851a8e4
jukki <jukkiamapparat@gmail.com>
2019-02-13 17:56:11 +0100

    Cleaned and TabLayout

--- app/src/main/java/com/example/pc/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/pc/myfirstapp/MainActivity.java
@@ -30,45 +30,16 @@ public class MainActivity extends AppCompatActivity {
     {
         String output = "";
         String tableFormat = "|%1$-14s|%2$-14s|%3$-14s|\r\n";
-        System.out.format(tableFormat, "A", "AA", "AAA");
-
 
         for (String[] x : inputList)
         {
-            /*
-            output = output + x[0] +" "+x[1]+" "+x[2]+" "+x[3]+" "+x[4] + "\r\n";
-            String.format("%16s%16s%16s%16s%16s", x[0], x[1], x[2],x[3],x[4]);
-            */
-            //int max = (x[0] > b) ? a : b; .substring(0,10)
             int max=0;
             int StringLength=12;
-            /*
-            output = output +String.format("%-14s%12s%-12s%-12s%-12s",
-                    x[0].substring(0, max = (x[0].length() < StringLength) ? x[0].length()-1 : StringLength),
-                    x[1].substring(0,  max = (x[1].length() < StringLength) ? x[1].length()-1 : StringLength),
-                    x[2].substring(0,  max = (x[2].length() < StringLength) ? x[2].length()-1 : StringLength),
-                    x[3].substring(0,  max = (x[3].length() < StringLength) ? x[3].length()-1 : StringLength),
-                    x[4].substring(0,  max = (x[4].length() < StringLength) ? x[4].length()-1 : StringLength)
-            ).trim()+ "\r\n";
-            */
 
-            /*
-            output = output +
-                    (x[0].substring(0, max = (x[0].length() < StringLength) ? x[0].length()-1 : StringLength) + "\t\t\t\t\t\t" +
-                    x[1].substring(0,  max = (x[1].length() < StringLength) ? x[1].length()-1 : StringLength)+ "\t\t" +
-                    x[2].substring(0,  max = (x[2].length() < StringLength) ? x[2].length()-1 : StringLength)+ "\t\t" +
-                    x[3].substring(0,  max = (x[3].length() < StringLength) ? x[3].length()-1 : StringLength))+ "\r\n";
-                    //x[4].substring(0,  StringLength)
-            */
             output = output + String.format(tableFormat,
                         x[0].substring(0, max = (x[0].length() < StringLength) ? x[0].length()-1 : StringLength),
                         x[1].substring(0,  max = (x[1].length() < StringLength) ? x[1].length()-1 : StringLength),
                         x[2].substring(0,  max = (x[2].length() < StringLength) ? x[2].length()-1 : StringLength));
-                            //x[3].substring(0,  max = (x[3].length() < StringLength) ? x[3].length()-1 : StringLength));
-            //x[4].substring(0,  StringLength)
-
-
-
         }
         return output;
     }
@@ -100,23 +71,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                /*
-                LayoutInflater myInflater = LayoutInflater.from(MainActivity.this);
-                View view = myInflater.inflate(R.layout.activity_main, null);
-                Toast mytoast = new Toast(MainActivity.this);
-                mytoast.setView(view);
-                mytoast.setDuration(Toast.LENGTH_LONG);
-                mytoast.show();
-                */
-                /*
-                Intent intent = new Intent(v.getContext(), ShowResultActivity.class);
-
-                EditText editText = (EditText) findViewById(R.id.FirstNumEditText);
-                String message = editText.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
-                */
-
                 Thread thread = new Thread(new Runnable() {
 
                     @Override
@@ -125,25 +79,19 @@ public class MainActivity extends AppCompatActivity {
                             String fullPage="";
                             ArrayList<String[]> stockList = new ArrayList<String[]>();
                             String[] stockData = new String[5];
-                            Integer stockCounter = 0;
 
                             try
                             {
-                                URL yahoo = new URL("https://www.finanzen.net/rohstoffe");
-                                BufferedReader in = new BufferedReader(
-                                        new InputStreamReader(
-                                                yahoo.openStream()));
+                                URL financeUrl = new URL("https://www.finanzen.net/rohstoffe");
+                                BufferedReader in = new BufferedReader(new InputStreamReader(financeUrl.openStream()));
 
                                 String inputLine;
                                 boolean tableStartTrigger = false;
-                                boolean dataStartTrigger = false;
                                 int fieldCounter=0;
-                                //String[] stockData = new String[3];
 
                                 while ((inputLine = in.readLine()) != null)
                                 {
                                     fullPage = fullPage + inputLine;
-                                    //if (inputLine.contains("<div class=\"row quotebox\">"))
 
                                     if (inputLine.contains("<table id=\"commodity_prices\" class=\"table\">"))
                                     {
@@ -152,18 +100,6 @@ public class MainActivity extends AppCompatActivity {
 
                                     if (tableStartTrigger)
                                     {
-                                        /**
-                                         *
-                                         <tr >
-                                         <td class="tdFixed" ><a href="/rohstoffe/goldpreis" title="Goldpreis">Goldpreis</a></td>
-                                         <td class="text-right tdFixed" >1.314,93</td>
-                                         <td class="text-right tdFixed" ><div class="arrow-right-top"></div></td>
-                                         <td class="text-right tdFixed" ><span class="colorGreen">1,14</span></td>
-                                         <td class="text-left tdFixed" >USD je Feinunze</td>
-                                         <td class="text-right tdFixed" >08.02 22:59</td>
-                                         </tr>
-                                         */
-
                                         if  (inputLine.contains("<td class"))
                                         {
                                             if (!removeHtmlTags(inputLine).equals("")) {
@@ -171,42 +107,20 @@ public class MainActivity extends AppCompatActivity {
                                                 fieldCounter++;
                                             }
                                         }
-
                                         if (inputLine.contains("</tr>") && fieldCounter>0)
                                         {
                                             stockList.add(stockData);
                                             stockData = new String[5];
                                             fieldCounter=0;
                                         }
-
                                         if(inputLine.contains("</table>"))
                                         {
                                             tableStartTrigger=false;
                                             break;
                                         }
-
-
-                                        // Einzelkurs
-                                        /*
-                                        if (inputLine.contains("<span>CHF</span>") && inputLine.indexOf("+")==-1 && inputLine.indexOf("-")==-1)
-                                        {
-                                            stockData[0] = inputLine.substring(0, inputLine.indexOf("<"));
-                                        }
-                                        if ((inputLine.indexOf("+")>=0 || inputLine.indexOf("-")>=0) && inputLine.contains("<span>CHF</span>"))
-                                        {
-                                            stockData[1] = inputLine.substring(0, inputLine.indexOf("<"));
-                                        }
-                                        if (inputLine.contains("<span>%</span>"))
-                                        {
-                                            stockData[2] = inputLine.substring(0, inputLine.indexOf("<"));
-                                            dataTrigger=false;
-                                            break;
-                                        }
-                                        */
                                     }
                                 }
                                 in.close();
-
                             }
                             catch (IOException e)
                             {
@@ -215,7 +129,8 @@ public class MainActivity extends AppCompatActivity {
                                 webTextView.setText(e.toString());
                             }
                             webTextView.setText(stocksToString(stockList));
-                        } catch (Exception e) {
+                        }
+                        catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -224,32 +139,11 @@ public class MainActivity extends AppCompatActivity {
                 thread.start();
                 Toast.makeText(getApplicationContext(), "HMMMMMMM", Toast.LENGTH_LONG).show();
 
-
                 EditText firstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
                 TextView resultTextView = (TextView) findViewById(R.id.ResultTextView);
 
                 String currentText = firstNumEditText.getText().toString();
                 resultTextView.setText(currentText);
-
-                /*
-                String messageStr="Hello Android!";
-                int server_port = 12345;
-                try
-                {
-
-
-                    DatagramSocket s = new DatagramSocket();
-                    InetAddress local = InetAddress.getByName("192.168.1.102");
-                    int msg_length=messageStr.length();
-                    byte[] message = messageStr.getBytes();
-                    DatagramPacket p = new DatagramPacket(message, msg_length,local,server_port);
-                    s.send(p);
-                }
-                catch (IOException e)
-                {
-                    System.out.println(e.toString());
-                }
-                */
             }
         });
     }
--- app/src/main/java/com/example/pc/myfirstapp/ShowResultActivity.java
+++ app/src/main/java/com/example/pc/myfirstapp/ShowResultActivity.java
@@ -3,6 +3,9 @@ package com.example.pc.myfirstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class ShowResultActivity extends AppCompatActivity {
@@ -17,7 +20,20 @@ public class ShowResultActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView2);
-        textView.setText(message);
+       // TextView textView = findViewById(R.id.textView2);
+        //textView.setText(message);
+        Integer[] test = {12,3,9001};
+        int counter=0;
+        TableLayout table = (TableLayout)ShowResultActivity.this.findViewById(R.id.stockTable);
+        for(Integer b :test)
+        {
+            // Inflate your row "template" and fill out the fields.
+            TableRow row = (TableRow)LayoutInflater.from(ShowResultActivity.this).inflate(R.layout.attrib_row, null);
+            ((TextView)row.findViewById(R.id.attrib_name)).setText(""+test[counter]);
+            ((TextView)row.findViewById(R.id.attrib_value)).setText(""+(test[counter]/2));
+            table.addView(row);
+            counter++;
+        }
+        table.requestLayout();
     }
 }
